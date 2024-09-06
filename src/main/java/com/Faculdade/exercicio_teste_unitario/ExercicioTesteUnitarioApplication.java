efe94ef52eeb828e7fba69ccc11c384ae865aecf
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-09-05 21:02:46 -0300

    Added base package

---
+++ src/main/java/com/Faculdade/exercicio_teste_unitario/ExercicioTesteUnitarioApplication.java
@@ -0,0 +1,13 @@
+package com.Faculdade.exercicio_teste_unitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioTesteUnitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioTesteUnitarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Faculdade/exercicio_teste_unitario/ExercicioTesteUnitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Faculdade.exercicio_teste_unitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioTesteUnitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
