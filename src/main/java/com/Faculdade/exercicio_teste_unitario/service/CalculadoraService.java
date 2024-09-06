dc573b49d5d39ddefca1c80ebef1e09bd02ba0fa
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-09-05 21:26:16 -0300

    Implements CalculadoraService

---
+++ src/main/java/com/Faculdade/exercicio_teste_unitario/service/CalculadoraService.java
@@ -0,0 +1,38 @@
+package com.Faculdade.exercicio_teste_unitario.service;
+
+import java.util.List;
+
+public class CalculadoraService {
+
+  public Float somaLista(List<Float> numeros) {
+    Float sum = 0.00f;
+    for (Float num : numeros) {
+      sum += num;
+    }
+    return sum;
+  }
+
+  public Float mediaLista(List<Float> numeros) {
+    Float sum = 0.00f;
+    for (Float num : numeros) {
+      sum += num;
+    }
+    Float media = sum / numeros.size();
+    return media;
+  }
+
+  public Float[] maiorMenorLista(List<Float> numeros) {
+    Float max = numeros.get(0);
+    Float min = numeros.get(0);
+
+    for (Float num : numeros) {
+      if (num > max) {
+        max = num;
+      } else if (num < min) {
+        min = num;
+      }
+
+    }
+    return new Float[] { max, min };
+  }
+}
