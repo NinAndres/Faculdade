358193585c2388dc87ffadbb98ba3b539243f5c2
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-09-05 21:48:40 -0300

    Implements CalculadoraTest

---
+++ src/test/java/com/Faculdade/exercicio_teste_unitario/CalculadoraServiceTest.java
@@ -0,0 +1,47 @@
+package com.Faculdade.exercicio_teste_unitario;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.Faculdade.exercicio_teste_unitario.service.CalculadoraService;
+
+@SpringBootTest
+public class CalculadoraServiceTest {
+
+  private CalculadoraService service;
+
+  @Test
+  @DisplayName("Funcao que testa a implementacao de somar da service")
+  public void testSomar() {
+
+    List<Float> numeros = Arrays.asList(1.1f, 2.2f, 3.3f, 4.4f, 5.5f);
+    assertEquals(16.5f, service.somaLista(numeros));
+    assertNotEquals(10.0f, service.somaLista(numeros));
+  }
+
+  @Test
+  @DisplayName("Funcao que testa a implementacao de media da service")
+  public void testMedia() {
+    List<Float> numeros = Arrays.asList(1.1f, 2.2f, 3.3f, 4.4f, 5.5f);
+    assertEquals(3.3f, service.mediaLista(numeros));
+    assertNotEquals(2.0f, service.mediaLista(numeros));
+  }
+
+  @Test
+  @DisplayName("Funcao que testa a implementacao de maior e menor da service")
+  public void testMaiorEMenor() {
+    List<Float> numeros = Arrays.asList(1.1f, 2.2f, 3.3f, 4.4f, 5.5f);
+    Float[] resultado = service.maiorMenorLista(numeros);
+    assertArrayEquals(new Float[] { 5.5f, 1.1f }, resultado);
+    assertNotEquals(new Float[] { 4.4f, 2.2f }, resultado);
+  }
+
+}
